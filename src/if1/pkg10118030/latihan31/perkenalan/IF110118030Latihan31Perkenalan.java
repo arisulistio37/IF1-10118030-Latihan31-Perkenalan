a1e9e392f92c86319dcdc0ecf6464ec145a526cc
arisulistio37 <arisulistio37@gmail.com>
2019-10-17 18:01:10 +0700

    Initial commit

---
+++ src/if1/pkg10118030/latihan31/perkenalan/IF110118030Latihan31Perkenalan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan31.perkenalan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ari Sulistio
+ * KELAS    : IF-1
+ * NIM      : 10118030
+ * Deskripsi Program : Program ini berisi tentang perkenalan mahasiswa
+ */
+public class IF110118030Latihan31Perkenalan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa kenal = new Mahasiswa();
+        kenal.perkenalanDiri("10118030", "Ari Sulistio");
+        kenal.perkenalanDiri("10118270", "Indra Tiola");
+        kenal.perkenalanDiri("10118271", "Robi Tanzil Gafeni");
+        kenal.perkenalanDiri("10118272", "Muhammad Nur Awaludin");
+        
+    }
+    
+}
---
+++ src/if1/pkg10118030/latihan31/perkenalan/Mahasiswa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan31.perkenalan;
+
+/**
+ *
+ * @author Ari Sulistio
+ */
+public class Mahasiswa {
+    public String nim , nama;
+    
+    public void perkenalanDiri(String nim, String nama){
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM is " +nim);
+        System.out.println("My Name is " +nama);
+        System.out.println("");
+    }
+}
